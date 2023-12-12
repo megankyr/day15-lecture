a2a7d3ac8caafc2f78c9dba4dd3b2133ab43940d
Megan <megan.koh96@gmail.com>
2023-12-12 19:58:37 +0800

    Initial Commit

--- src/main/java/com/ssf/day15lecture/controller/IndexController.java
+++ src/main/java/com/ssf/day15lecture/controller/IndexController.java
@@ -1,15 +1,12 @@
 package com.ssf.day15lecture.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ssf.day15lecture.model.Item;
     @Controller
     @RequestMapping("/")
     public class IndexController {
-        public String getIndex(Model model) {
-            model.addAttribute("item", new Item());
+        public String getIndex() {
             return "index";
         }
     }
